8f5ee328872a64c0d94cf3afc890f4c7bcb4029f
Stefano Rodriguez <sr@bendingspoons.com>
2018-02-07 16:00:21 +0100

    Add onPushed to push

--- packages/flutter/lib/src/widgets/navigator.dart
+++ packages/flutter/lib/src/widgets/navigator.dart
@@ -1433,7 +1433,7 @@ class NavigatorState extends State<Navigator> with TickerProviderStateMixin {
   /// }
   /// ```
   @optionalTypeArgs
-  Future<T> push<T extends Object>(Route<T> route) {
+  Future<T> push<T extends Object>(Route<T> route, { VoidCallback onPushed }) {
     assert(!_debugLocked);
     assert(() { _debugLocked = true; return true; }());
     assert(route != null);
@@ -1442,7 +1442,10 @@ class NavigatorState extends State<Navigator> with TickerProviderStateMixin {
     route._navigator = this;
     route.install(_currentOverlayEntry);
     _history.add(route);
-    route.didPush();
+    route.didPush().whenCompleteOrCancel(() {
+      if (onPushed != null)
+        onPushed();
+    });
     route.didChangeNext(null);
     if (oldRoute != null)
       oldRoute.didChangeNext(route);
