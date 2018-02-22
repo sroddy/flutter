570bde672129431b4875fad2d8ee55aa449a9542
Stefano Rodriguez <sr@bendingspoons.com>
2018-02-22 16:29:24 +0100

    Fix algo

--- packages/flutter/lib/src/painting/text_painter.dart
+++ packages/flutter/lib/src/painting/text_painter.dart
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 import 'dart:ui' as ui show Paragraph, ParagraphBuilder, ParagraphConstraints, ParagraphStyle;
-import 'dart:math' as math show max;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
@@ -371,17 +370,15 @@ class TextPainter {
       if (newWidth != width)
         _paragraph.layout(new ui.ParagraphConstraints(width: newWidth));
       final List<TextBox> boxes = _paragraph.getBoxesForRange(0, text.toPlainText().length);
-      final Map<double, double> lineWidths = <double, double>{};
+      double startMost = double.maxFinite;
+      double endMost = 0.0;
       for (final TextBox box in boxes) {
-        if (lineWidths[box.top] == null) {
-          lineWidths[box.top] = 0.0;
-        }
-        lineWidths[box.top] += box.end - box.start;
+        if (startMost > box.start)
+          startMost = box.start;
+        if (endMost < box.end)
+          endMost = box.end;
       }
-      final double maxBoxWidth = lineWidths.values.fold(
-        0.0,
-        (prev, elem) => math.max(prev, elem),
-      );
+      final double maxBoxWidth = endMost - startMost;
       if (maxBoxWidth > 0.0 && maxBoxWidth >= minWidth && maxBoxWidth != width) {
         _paragraph.layout(new ui.ParagraphConstraints(width: maxBoxWidth));
       }
